c30c2049d3082b8936716fa80a92f144b9d88f67
Kenny Caballero <caballerokenny3@gmail.com>
2024-11-15 02:30:46 -0500

    agregar listas

--- src/app/ejemplo-directivas/ejemplo-directivas.component.ts
+++ src/app/ejemplo-directivas/ejemplo-directivas.component.ts
@@ -11,11 +11,21 @@ export class EjemploDirectivasComponent {
   mostrarAlumnos = false;
   mostrarCursos = false;
 
+  alumnosLista: Alumno = []
+  cursosLista: Curso = []
+
   verAlumnos(): void {
     this.mostrarAlumnos = true;
+    this.alumnosLista.push({id: 1, nombre: 'kenny', apellido: 'caballero'});
+    this.alumnosLista.push({id: 2, nombre: 'harold', apellido: 'caballero'});
+    this.alumnosLista.push({id: 3, nombre: 'matia', apellido: 'caballero'});
+    this.alumnosLista.push({id: 4, nombre: 'julio', apellido: 'caballero'});
   }
 
   verCursos(): void {
     this.mostrarCursos = true;
   }
 }
+
+type Alumno = Array<{id: number; nombre: string; apellido: string}>
+type Curso = Array<{id: number; nombre: string; precio: number}>
